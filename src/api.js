f576376f31748c827ed5130ccc0b4f823dd32d7e
Santos Estrada <santos.estrada1@gmail.com>
2020-08-05 11:31:42 -0500

    Wednesday push 2

--- src/api.js
+++ src/api.js
@@ -1,7 +1,38 @@
-module.exports = {
-  getMovies: () => {
-    return fetch('/api/movies')
-      .then(response => response.json());
+const $ = require('jquery');
 
-  }
+// VARIABLE DECLARATIONS
+let selectedMovie = {
+  id: 0
+};
+
+const getMovies = () => {
+  return fetch('/api/movies')
+      .then(response => {
+        return response.json();
+      });
+};
+
+const displayAllMovies = () => {
+  getMovies().then((movies) => {
+    $('.movie-list').html(createMovieString(movies));
+    activateMovieList();
+    Promise.resolve().then(response => "Success: " + response);
+  }).catch((error) => {
+    alert('Oh no! Something went wrong.\nCheck the console for details.');
+    console.log(error);
+  });
+};
+
+  module.exports = {
+    selectedMovie,
+    getMovies,
+    displayAllMovies,
+    createMovieString,
+    addMovie,
+    editMovie,
+    deleteMovie,
+    searchMovies,
+    renderSearchResults,
+    toggleDisable,
+    renderLoading
 };
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 //allows jquery to be used
-const $ = require('jquery');
+// const $ = require('jquery');
 
 
 /**
@@ -12,26 +12,48 @@ sayHello('World');*/
 /**
  * require style imports
  */
+//
+// const {getMovies} = require('./api.js');
+//
+// getMovies().then((movies) => {
+//   // console.log('Here are all the movies:');
+//   movies.forEach(({title, rating, id, genre}) => {
+//     // console.log(`id#${id} - ${title} - rating: ${rating} - genre: ${genre}`);
+//
+//   });
+//
+//
+// })
+//     .catch((error) => {
+//   alert('Oh no! Something went wrong.\nCheck the console for details.')
+//   console.log(error);
+// });
 
-const {getMovies} = require('./api.js');
+// VARIABLE DECLARATIONS
 
-getMovies().then((movies) => {
-  // console.log('Here are all the movies:');
-  movies.forEach(({title, rating, id, genre}) => {
-    // console.log(`id#${id} - ${title} - rating: ${rating} - genre: ${genre}`);
+let selectedMovie = {
+  id: 0
+};
 
+const getMovies = () => {
+  return fetch('/api/movies')
+      .then(response => {
+        return response.json();
+      });
+};
+
+const displayAllMovies = () => {
+  getMovies().then((movies) => {
+    $('.movie-list').html(createMovieString(movies));
+    activateMovieList();
+    Promise.resolve().then(response => "Success: " + response);
+  }).catch((error) => {
+    alert('Oh no! Something went wrong.\nCheck the console for details.');
+    console.log(error);
   });
+};
 
 
-})
-    .catch((error) => {
-  alert('Oh no! Something went wrong.\nCheck the console for details.')
-  console.log(error);
-});
-
-
-
-let outputMovies = "";
 
 
 
